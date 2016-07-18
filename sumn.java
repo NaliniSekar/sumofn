1eca6f65e355bffd655bd98635b942cad2e79b02
NaliniSekar <NaliniSekar@users.noreply.github.com>
2016-07-18 02:10:46 -0700

    Update and rename README.md to sumn.java

---
+++ sumn.java
@@ -0,0 +1,15 @@
+# sumofn
+class a
+{
+Public static void main(String args[])
+{
+System.out.println("enter num");
+Scanner in = new Scanner();
+int b=in.nextInt();
+for(int n=0;i<b;i++)
+{
+int sum=sum+i;
+System.out.println("sum is"+sum);
+}
+}
+}
